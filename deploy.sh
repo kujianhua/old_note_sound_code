2f4b8cba97505c709083ed3cc16ffb2b2c5acaba
库建华 <kujh@health-100.cn>
2021-11-10 09:30:29 +0800

    2021年11月10日09:30:29

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
  # @Author: 库建华
  # @Date: 2020-03-30 23:08:55
  # @LastEditors: Please set LastEditors
- # @LastEditTime: 2021-11-10 09:11:37
+ # @LastEditTime: 2021-11-10 09:30:02
  # @Version: 1
  # @Description:
  ###
@@ -29,7 +29,8 @@ git commit -m `date +%Y年%m月%d日%H:%M:%S`
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
- git push -f https://github.com/kujianhua/my_note.git master:gh-pages
+#  git push -f https://github.com/kujianhua/my_note.git master:gh-pages
+ git push -f git@github.com:kujianhua/my_note.git master:gh-pages
 
 cd -
 
